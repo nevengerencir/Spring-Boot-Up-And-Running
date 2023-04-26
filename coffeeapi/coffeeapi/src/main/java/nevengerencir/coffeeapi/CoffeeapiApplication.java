606082443fde7ea26ba7a14f365da974c31df33c
NevenGerencir <gerencirn@gmail.com>
2023-04-26 11:35:17 +0200

    A simple rest api in springboot

---
+++ coffeeapi/coffeeapi/src/main/java/nevengerencir/coffeeapi/CoffeeapiApplication.java
@@ -0,0 +1,13 @@
+package nevengerencir.coffeeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeapiApplication.class, args);
+	}
+
+}
